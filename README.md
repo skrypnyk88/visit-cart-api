412bf3dd86afaf071d8432408c450f8868d3cf62
Dmytro <skrypnyk_d@ukr.net>
2017-07-21 09:40:15 +0300

    change migrate

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# README
+1) git clone git@github.com:skrypnyk88/visit-cart-api.git
+2) cd visit-cart-api
+3) bundle install
+4) rails db:migrate (Elasticsearch повинен бути запущений локально)
+5) rails db:seed (Elasticsearch повинен бути запущений локально)
+6) rails s
+
+На хероку відсутня можливість використовувати Elasticsearch і деплоїти фото.
--- db/migrate/20170720101632_add_devise_to_users.rb
+++ db/migrate/20170720101632_add_devise_to_users.rb
@@ -3,11 +3,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
     change_table :users do |t|
       t.string :email, null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
-      t.add_column :sign_in_count, :integer, default: 0
-      t.add_column :current_sign_in_at, :datetime
-      t.add_column :last_sign_in_at, :datetime
-      t.add_column :current_sign_in_ip, :string
-      t.add_column :last_sign_in_ip, :string
 
       add_index :users, :email, unique: true
     end
